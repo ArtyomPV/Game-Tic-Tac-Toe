a34a54cbd672547c7ddf43aa0f544162a0551c44
Artyom <itcoding76@gmail.com>
2023-02-28 08:24:41 +0200

    стартовое поле цикла игры

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ desk = [[' ', '0', '1', '2'],
         ['0', '-', '-', '-'],
         ['1', '-', '-', '-'],
         ['2', '-', '-', '-']]
+count_steps = 9
 
 def showDesk(desk):
     for i in range(4):
         print(*desk[i])
+
+while count_steps:
+    # start code
+
+    # end code
+    count_steps -= 1
 
 showDesk(desk)
